313998c8d7be07ba6f2000e58d43af6c27217fc3
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-10 15:05:26 +0200

    Updated the image

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ function LoginPage({ saveUsername }) {
           alt={'Login Image'}
           objectFit={'cover'}
           src={
-            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+            'https://images.unsplash.com/photo-1611679782010-5ac7ff596d9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
           }
         />
       </Flex>
