23a0585ddeb5dce7c260240e2fd0e878992b4e58
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-10 14:59:26 +0200

    Updated the ui

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { useEffect } from 'react';
 
 import { connectWithSocket } from '../utils/wssConnection/wssConnection';
+import Head from 'next/head';
 import store from '../redux/store';
 
 function MyApp({ Component, pageProps }) {
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <ReduxProvider store={store}>
+        <Head>
+          <title>WebRTC VideoChat project</title>
+        </Head>
         <Component {...pageProps} />
       </ReduxProvider>
     </ChakraProvider>
---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shorcut icon" href="/logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
